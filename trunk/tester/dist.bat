26b13adf279aca8b05da08b687734692294f5d61
sven.hecht <sven.hecht@35ccbd06-6266-11de-9e79-e94433246604>
2009-07-23 21:11:56 +0000

    - improved deployment

--- trunk/tester/dist.bat
+++ trunk/tester/dist.bat
@@ -1,1 +1,19 @@
+
+REM Build Exe
 python setup.py py2exe
+
+REM Copy resources
+copy /Y img\*.png dist\img\
+copy /Y maps\*.map dist\maps\
+copy /Y F:\WINDOWS\system32\GLU32.DLL dist\
+copy /Y F:\WINDOWS\system32\OPENGL32.DLL dist\
+copy /Y F:\WINDOWS\system32\msvcrt.dll dist\
+
+REM pack Release
+copy /Y zip.exe dist
+cd dist
+zip -R release.zip * -x zip.exe
+copy /Y release.zip ..
+cd ..
+
+pause
